e1b40a01f66b9f5f35fee1404b56654d277f0507
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-08-03 10:01:56 +0530

    Script to load from cache if network request fails

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -1,2 +1,14 @@
-data=$(curl "https://api.openweathermap.org/data/2.5/forecast?lat=12.9716&lon=77.5946&appid=$1&units=metric") 
-echo $data > "data/weather.json"
+#!/bin/bash
+
+
+while [ "$(hostname -i | grep '192.168.0.100')" == "" ]; do
+    cat "/home/vbvnyk/.config/eww/scripts/data/weather_copy.json" >"/home/vbvnyk/.config/eww/scripts/data/weather.json"
+    sleep 1
+done
+
+# Fetch weather data using the provided API key
+data=$(curl "https://api.openweathermap.org/data/2.5/forecast?lat=12.9716&lon=77.5946&appid=$1&units=metric")
+
+# Write the fetched data to a JSON file
+echo "$data" >"/home/vbvnyk/.config/eww/scripts/data/weather.json"
+echo "$data" >"/home/vbvnyk/.config/eww/scripts/data/weather_copy.json"
