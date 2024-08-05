ed4d64625ddee2b529e6d10794201158b80682cf
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2024-08-05 10:28:58 +0530

    Add transition for background color

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -7,7 +7,7 @@ while [ "$(hostname -i | grep '192.168.0.100')" == "" ]; do
 done
 
 # Fetch weather data using the provided API key
-data=$(curl "https://api.openweathermap.org/data/2.5/forecast?lat=12.9716&lon=77.5946&appid=$1&units=metric")
+data=$(curl "https://api.openweathermap.org/data/2.5/forecast?lat=13.0055&lon=77.5692&appid=$1&units=metric")
 
 # Write the fetched data to a JSON file
 echo "$data" >"/home/vbvnyk/.config/eww/scripts/data/weather.json"
